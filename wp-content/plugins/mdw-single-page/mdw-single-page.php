1b2e4363d77f6c9c02a51ef0447e1f181f309be9
erikdmitchell <erikdmitchell@gmail.com>
2015-08-25 09:34:17 -0400

    created a tru front page

--- wp-content/plugins/mdw-single-page/mdw-single-page.php
+++ wp-content/plugins/mdw-single-page/mdw-single-page.php
@@ -141,7 +141,10 @@ class MDWThemeSinglePage {
 		//$org_template_filename=get_page_template_slug($post->ID);
 		$plugin_template_folder=plugin_dir_path(__FILE__).$this->template_folder_name;
 		$user_template_folder=get_stylesheet_directory().'/'.$this->template_folder_override_name;
-//echo "tp: $template_page<br>";
+
+		//if (WP_DEBUG === true)
+			//echo "template: $template_page<br>";
+
 		// check for custom user files //
 		if (file_exists($user_template_folder.'/'.$template_page)) :
 			$new_template=$user_template_folder.'/'.$template_page;
