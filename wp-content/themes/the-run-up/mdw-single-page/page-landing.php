4768c8e09a0f0e9a6935c80052925aa1f39eb62c
Erik Mitchell <erikdmitchell@gmail.com>
2015-09-05 09:00:02 -0400

    synched with live site

---
+++ wp-content/themes/the-run-up/mdw-single-page/page-landing.php
@@ -0,0 +1,10 @@
+<div class="bg-header"></div>
+<section class="content-block home section-1" id="home">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<?php echo get_home_content(); ?>
+			</div>
+		</div><!-- .row -->
+	</div><!-- .container -->
+</section><!-- .section -->
