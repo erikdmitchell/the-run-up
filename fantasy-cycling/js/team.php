96fec02325b6d75c9be2c87e5e58012ba8f28d55
erikdmitchell <erikdmitchell@gmail.com>
2016-08-26 11:04:27 -0400

    basic team js tempalte build out

--- fantasy-cycling/js/team.php
+++ fantasy-cycling/js/team.php
@@ -6,39 +6,78 @@
 ?>
 
 <script type="text/template" id="tmpl-fc-rider-table">
-	<div class="riders-table">
-		<div class="row rider header">
-			<div class="col-sm-3 name">Name</div>
-			<div class="col-sm-2 cost">Cost</div>
-			<div class="col-sm-1 points">Points</div>
-			<div class="col-sm-1 rank">Rank</div>
-			<div class="col-sm-2 last-year">Last Year</div>
-			<div class="col-sm-3 last-race">Last Race</div>
+	<div class="container">
+
+		<div class="row budget">
+			<div class="col-xs-3 col-sm-offset-7 col-sm-2 text">Budget:</div>
+			<div class="col-xs-offset-6 col-xs-3 col-sm-offset-0 amount"><?php echo fantasy_cycling_format_cost($user_team->budget); ?></div>
 		</div>
 
-		<div class="rider-list"></div>
+		<div class="riders-table">
+
+			<div class="hidden-sm hidden-md hidden-lg row stats header">
+				<div class="col-xs-2 rank">Rank</div>
+				<div class="col-xs-2 points">Points</div>
+				<div class="col-xs-3 last-year">Last Year</div>
+				<div class="col-xs-5 last-race">Last Race</div>
+			</div>
+
+			<div class="hidden-xs row header smplus">
+				<div class="col-sm-1">&nbsp;</div>
+				<div class="col-sm-4 name">Name</div>
+				<div class="col-sm-1 proj">Proj.</div>
+				<div class="col-sm-1 rank">Rank</div>
+				<div class="col-sm-1 points">Points</div>
+				<div class="col-sm-1 last-year">Last Year</div>
+				<div class="col-sm-1 last-race">Last Race</div>
+				<div class="col-sm-2 cost">Cost</div>
+			</div>
+
+			<div class="rider-list"></div>
+
+		</div>
 
 		<div id="rider-list-loading-more">loading more...</div>
+
 	</div>
 </script>
 
 <script type="text/template" id="tmpl-fc-rider-row">
-	<div id="rider-<%= id %>" class="row rider">
-		<div class="col-sm-3">
-			<span class="name">
+	<div id="rider-<%= id %>" class="rider">
+		<div class="hidden-sm hidden-md hidden-lg row actions">
+			<div class="col-xs-2">
 				<% if (onTeam) { %>
-				  <%= name %>
-				<% } else { %>
-					<a href="#" class="add-rider" data-id="<%= id %>"><%= name %></a>
-					<p><a href="<%= url %>" class="rider-more">more info</a></p>
+					<a href="#" class="add-rider" data-id="<%= id %>"><i class="fa fa-plus-circle" aria-hidden="true"></i></a>
 				<% } %>
-			</span>
+			</div>
+			<div class="col-xs-7 name"><%= name %> FLAG</div>
+			<div class="col-xs-3 cost"><%= cost %></div>
 		</div>
 
-		<div class="col-sm-2 cost"><%= cost %></div>
-		<div class="col-md-1 points"><%= points %></div>
-		<div class="col-sm-1 rank"><%= rank %></div>
-		<div class="col-sm-2 last-year"><%= lastYearResult.place %></div>
-		<div class="col-sm-3 last-race"><span><%= last_race.place %></span> <a href="<%= last_race.url %>"><%= last_race.event %></a></div>
+		<div class="hidden-sm hidden-md hidden-lg row stats">
+			<div class="col-xs-2 rank"><%= rank %></div>
+			<div class="col-xs-2 points"><%= points %></div>
+			<div class="col-xs-3 last-year"><%= lastYearResult.place %></div>
+			<div class="col-xs-5 last-race">
+				<%= last_race.place %> (<a href="<%= last_race.url %>"><%= last_race.event %></a>)
+			</div>
+		</div>
+
+		<div class="row hidden-xs smplus">
+			<div class="col-sm-1">
+				<% if (onTeam) { %>
+					<a href="#" class="add-rider" data-id="<%= id %>"><i class="fa fa-plus-circle" aria-hidden="true"></i></a>
+				<% } else { %>
+					<div class="empty-add-rider">N</div>
+				<% } %>
+			</div>
+			<div class="col-sm-4 name"><%= name %> FLAG</div>
+			<div class="col-sm-1 proj">PREDIC</div>
+			<div class="col-sm-1 rank"><%= rank %></div>
+			<div class="col-sm-1 points"><%= points %></div>
+			<div class="col-sm-1 last-year"><%= lastYearResult.place %></div>
+			<div class="col-sm-1 last-race"><%= last_race.place %></div>
+			<div class="col-sm-2 cost"><%= cost %></div>
+		</div>
 	</div>
 </script>
