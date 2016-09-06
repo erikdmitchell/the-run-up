7dd1a1f53048ac4dbf319cc3977ea1959669d223
erikdmitchell <erikdmitchell@gmail.com>
2016-09-06 11:29:18 -0400

    updating my team and team templates

--- fantasy-cycling/js/team.php
+++ fantasy-cycling/js/team.php
@@ -3,16 +3,9 @@
  * template for add rider modal content
  * these are backbone tempaltes
  */
-
-global $fantasy_cycling_user_team;
 ?>
 
 <script type="text/template" id="tmpl-fc-rider-table">
-
-	<div class="row budget">
-		<div class="col-xs-3 col-sm-offset-7 col-sm-2 text">Budget:</div>
-		<div class="col-xs-offset-6 col-xs-3 col-sm-offset-0 amount"><?php echo fantasy_cycling_format_cost($fantasy_cycling_user_team->budget); ?></div>
-	</div>
 
 	<div class="riders-table">
 
@@ -48,9 +41,10 @@ global $fantasy_cycling_user_team;
 			<div class="col-xs-2">
 				<% if (!onTeam) { %>
 					<a href="#" class="add-rider" data-id="<%= id %>"><i class="fa fa-plus-circle" aria-hidden="true"></i></a>
+<!-- 					<a href="#" class="add-rider" data-id="<%= id %>"><span><%= name %></span></a> <%= flag %> -->
 				<% } %>
 			</div>
-			<div class="col-xs-7 name"><%= name %> <%= flag %></div>
+			<div class="col-xs-7 name"><span><%= name %></span> <%= flag %></div>
 			<div class="col-xs-3 cost"><%= cost %></div>
 		</div>
 
@@ -67,8 +61,9 @@ global $fantasy_cycling_user_team;
 			<div class="col-sm-1">
 				<% if (!onTeam) { %>
 					<a href="#" class="add-rider" data-id="<%= id %>"><i class="fa fa-plus-circle" aria-hidden="true"></i></a>
+<!-- 					<a href="#" class="add-rider" data-id="<%= id %>"><span><%= name %></span></a> <%= flag %> -->
 				<% } else { %>
-					<div class="empty-add-rider">N</div>
+					<div class="empty-add-rider"></div>
 				<% } %>
 			</div>
 			<div class="col-sm-4 name"><%= name %> <%= flag %></div>
@@ -79,5 +74,93 @@ global $fantasy_cycling_user_team;
 			<div class="col-sm-1 last-race"><%= last_race.place %></div>
 			<div class="col-sm-2 cost"><%= cost %></div>
 		</div>
+	</div>
+</script>
+
+<script type="text/template" id="tmpl-fc-my-team-list">
+	<div class="row header">
+		<div class="col-xs-2 col-sm-1">&nbsp;</div>
+		<div class="col-xs-6 col-sm-4 name">Name</div>
+		<div class="col-xs-2 cost">Cost</div>
+		<div class="col-xs-2 col-sm-1 proj">Proj.</div>
+		<div class="hidden-xs col-sm-1 rank">Rank</div>
+		<div class="hidden-xs col-sm-1 points">Points</div>
+		<div class="hidden-xs col-sm-1 last-year">Last Year</div>
+		<div class="hidden-xs col-sm-3 last-race">Last Race</div>
+	</div>
+
+	<div id="team-riders"></div>
+</script>
+
+<script type="text/template" id="tmpl-fc-team-rider-row">
+	<div id="rider-row-<%= rowID %>" class="row rider"></div>
+</script>
+
+<script type="text/template" id="tmpl-fc-team-rider-row-content">
+
+	<% if (id) { %>
+		<div class="col-xs-2 col-sm-1 add-remove">
+			<span class="add-remove">
+				<a href="">
+					<i class="fa add-rider" aria-hidden="true">
+						<button class="tru-add-riders-btn">Add Rider</button>
+					</i>
+					<i class="fa fa-minus-circle remove-rider" aria-hidden="true"></i>
+				</a>
+			</span>
+		</div>
+
+		<div class="col-xs-6 col-sm-4 name">
+			<a href="<%= url %>"><%= name %></a>
+			<span class="country"><%= flag %></span>
+		</div>
+
+	<% } else { %>
+		<div class="col-xs-12 add-remove">
+			<span class="add-remove">
+				<a href="">
+					<i class="fa add-rider" aria-hidden="true">
+						<button class="tru-add-riders-btn">Add Rider</button>
+					</i>
+					<i class="fa fa-minus-circle remove-rider" aria-hidden="true"></i>
+				</a>
+			</span>
+		</div>
+	<% } %>
+
+	<div class="col-xs-2 cost">
+		<% if (cost) { %>
+			$
+		<% } %>
+		<%= cost %>
+	</div>
+
+	<div class="col-xs-1 col-sm-1 proj"><%= projectedPlace %></div>
+	<div class="hidden-xs col-sm-1 rank"><%= rank.rank %> <%= rank.prev_icon %></div>
+	<div class="hidden-xs col-sm-1 points"><%= rank.points %></div>
+	<div class="hidden-xs col-sm-1 last-year"><%= last_year_result.place %></div>
+	<div class="hidden-xs col-sm-3 last-race"><span><%= last_result.place %></span> <a href="<%= last_result.url %>"><%= last_result.event %></a></div>
+
+	<input type="hidden" class="rider-id" value="<%= id %>" />
+</script>
+
+<script type="text/template" id="tmpl-fc-rider-row">
+	<div id="rider-<%= id %>" class="row rider">
+		<div class="col-sm-3">
+			<span class="name">
+				<% if (onTeam) { %>
+				  <%= name %> <%= flag %>
+				<% } else { %>
+					<a href="#" class="add-rider" data-id="<%= id %>"><%= name %></a> <%= flag %>
+					<p><a href="<%= url %>" class="rider-more">more info</a></p>
+				<% } %>
+			</span>
+		</div>
+
+		<div class="col-sm-2 cost">$<%= cost %></div>
+		<div class="col-md-1 points"><%= points %></div>
+		<div class="col-sm-1 rank"><%= rank %></div>
+		<div class="col-sm-2 last-year"><%= lastYearResult.place %></div>
+		<div class="col-sm-3 last-race"><span><%= last_race.place %></span> <a href="<%= last_race.url %>"><%= last_race.event %></a></div>
 	</div>
 </script>
--- fantasy-cycling/my_team.php
+++ fantasy-cycling/my_team.php
@@ -10,11 +10,12 @@ get_header();
 
 	<h1 class="page-title"><?php echo $fantasy_cycling_user_team->name; ?></h1>
 
+	<?php fc_my_team_message(); ?>
+
 	<div class="row">
 		<div class="col-xs-12 col-sm-4">
 			<div class="budget">Budget: <span class="amount"><?php echo fantasy_cycling_format_cost($fantasy_cycling_user_team->budget); ?></span></div>
 
-			<?php fc_my_team_message(); ?>
 			<div id="my-team-ajax"></div>
 		</div>
 		<div class="col-xs-12 col-sm-8">
@@ -25,51 +26,9 @@ get_header();
 		</div>
 	</div>
 
-	<div class="my-team-list">
-		<div class="row header">
-			<div class="col-xs-2 col-sm-1">&nbsp;</div>
-			<div class="col-xs-8 col-sm-4 name">Name</div>
-			<div class="col-xs-2 col-sm-1 proj">Proj.</div>
-			<div class="hidden-xs col-sm-1 rank">Rank</div>
-			<div class="hidden-xs col-sm-1 points">Points</div>
-			<div class="hidden-xs col-sm-1 last-year">Last Year</div>
-			<div class="hidden-xs col-sm-3 last-race">Last Race</div>
-		</div>
+	<div id="my-team-list" class="my-team-list"></div>
 
-		<?php foreach ($fantasy_cycling_user_team->riders as $key => $rider) : ?>
-
-			<div id="rider-row-<?php echo $key; ?>" class="row rider">
-				<?php if ($rider->id) : ?>
-
-					<div class="col-xs-2 col-sm-1 add-remove">
-						<?php fantasy_cycling_display_add_remove($rider); ?>
-					</div>
-					<div class="col-xs-8 col-sm-4 name">
-						<a href="<?php fantasy_cycling_rider_link($rider->slug); ?>"><?php echo $rider->name; ?></a>
-						<span class="country"><?php fantasy_cycling_flag($rider->nat); ?></span>
-					</div>
-					<div class="col-xs-2 col-sm-1 proj"><?php echo fantasy_cycling_get_rider_place_prediction($fantasy_cycling_next_race->id, $rider->id); ?></div>
-				<?php else :?>
-
-					<div class="col-xs-12 add-remove">
-						<?php fantasy_cycling_display_add_remove($rider); ?>
-					</div>
-
-				<?php endif; ?>
-
-				<div class="hidden-xs col-sm-1 rank"><?php echo $rider->rank->rank; ?><?php echo $rider->rank->prev_icon; ?></div>
-				<div class="hidden-xs col-sm-1 points"><?php echo $rider->rank->points; ?></div>
-				<div class="hidden-xs col-sm-1 last-year"><?php echo $rider->last_year_result->place; ?></div>
-				<div class="hidden-xs col-sm-3 last-race">
-					<?php if ($rider->last_result->code) : ?>
-						<?php echo $rider->last_result->place; ?> (<a href="<?php echo fantasy_cycling_race_link($rider->last_result->code); ?>"><?php echo fc_trim_string($rider->last_result->event, 18); ?></a>)
-					<?php endif; ?>
-				</div>
-
-				<input type="hidden" class="rider-id" value="<?php echo $rider->id; ?>" />
-			</div>
-		<?php endforeach; ?>
-	</div>
+	<div class="time-notice">* All times EST - current time is <?php echo date(get_option('time_format'), strtotime(current_time('mysql'))); ?></div>
 
 </div>
 
