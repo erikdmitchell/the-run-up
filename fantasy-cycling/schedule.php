f047ef172b7f4dadaf4799eaab133286292e61eb
erikdmitchell <erikdmitchell@gmail.com>
2016-09-28 15:31:30 -0400

    updated schdule template

--- fantasy-cycling/schedule.php
+++ fantasy-cycling/schedule.php
@@ -7,22 +7,22 @@ get_header(); ?>
 
 <div class="fantasy-cycling-schedule fc-template container">
 
-	<h1 class="page-title">Schedule</h1>
+	<h1 class="page-title">Upcoming Races</h1>
 
 		<div class="fc-races">
 
 			<div class="header row">
-				<div class="col-xs-4 col-sm-3 date">Date</div>
+				<div class="col-xs-4 col-sm-2 date">Date</div>
 				<div class="col-xs-8 race">Race</div>
 			</div>
 
 			<?php foreach ($fantasy_cycling_schedule as $race) : ?>
 				<div id="race-<?php echo $race->id; ?>" class="row">
-					<div class="col-xs-4 col-sm-3 date">
-						<?php echo date('M. j, Y', strtotime($race->date)); ?>
+					<div class="col-xs-4 col-sm-2 date">
+						<?php fc_race_date_formatted($race->date); ?>
 					</div>
 					<div class="col-xs-8 race">
-						<?php echo $race->name; ?>
+						<a href="<?php echo fantasy_cycling_get_race_link($race->code); ?>"><?php echo $race->name; ?></a>
 					</div>
 				</div>
 			<?php endforeach; ?>
