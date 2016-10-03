49d89f0b53c240d4c805ea593e22936914e6d3f5
erikdmitchell <erikdmitchell@gmail.com>
2016-10-03 16:11:44 -0400

    https for google script

--- functions.php
+++ functions.php
@@ -11,7 +11,7 @@ function tru_scripts_styles() {
 	wp_enqueue_script('tru-front-page-script', get_stylesheet_directory_uri().'/js/front-page.js', array('jquery'), '0.1.0', true);
 	wp_enqueue_script('tru-team-script', get_stylesheet_directory_uri().'/js/team.js', array('jquery'), '0.1.0', true);
 
-	wp_enqueue_style('google-fonts-arvo', 'http://fonts.googleapis.com/css?family=Arvo:400,700,400italic');
+	wp_enqueue_style('google-fonts-arvo', 'https://fonts.googleapis.com/css?family=Arvo:400,700,400italic');
 	wp_enqueue_style('parent-style', get_template_directory_uri().'/style.css');
 }
 add_action('wp_enqueue_scripts', 'tru_scripts_styles');
