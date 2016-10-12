675c9a21895cd315fc644e443ce40066a2c6cc5f
erikdmitchell <erikdmitchell@gmail.com>
2016-10-12 11:52:19 -0400

    rider header template updated

--- fantasy-cycling/js/rider-header.php
+++ fantasy-cycling/js/rider-header.php
@@ -10,11 +10,22 @@
 ?>
 
 <script type="text/template" id="tmpl-fc-rider-header">
-	<div class="em-col-sm-4 name">Name</div>
-	<div class="em-col-sm-2 fantasy-points">Fantasy Points</div>
-	<div class="em-col-sm-2 uci-points">UCI Points</div>
-	<div class="em-col-sm-1 rank">Rank</div>
-	<div class="em-col-sm-1 last-race">Last Race</div>
-	<div class="em-col-sm-1 perc-owned">Percent Owned</div>
-	<div class="em-col-sm-1 cost">Cost</div>
+
+	<div class="hidden-sm hidden-md hidden-lg stats header">
+		<div class="col-xs-3 fantasy-points">Fantasy Points</div>
+		<div class="col-xs-3 rank">Rank</div>
+		<div class="col-xs-3 last-race">Last Race</div>
+		<div class="col-xs-3 perc-owned">Percent Owned</div>
+	</div>
+
+	<div class="hidden-xs header smplus">
+		<div class="col-sm-4 name">Name</div>
+		<div class="col-sm-2 fantasy-points">Fantasy Points</div>
+		<div class="col-sm-2 uci-points">UCI Points</div>
+		<div class="col-sm-1 rank">Rank</div>
+		<div class="col-sm-1 last-race">Last Race</div>
+		<div class="col-sm-1 perc-owned">Percent Owned</div>
+		<div class="col-sm-1 cost">Cost</div>
+	</div>
+
 </script>
