fdb27f80f4eedb91704b717044c4cf1b40a8467b
erikdmitchell <erikdmitchell@gmail.com>
2016-10-12 12:28:48 -0400

    styled my team page mobile

--- fantasy-cycling/js/my-team-list.php
+++ fantasy-cycling/js/my-team-list.php
@@ -1,13 +1,13 @@
 <script type="text/template" id="tmpl-fc-my-team-list">
 	<div class="row header">
 		<div class="col-xs-2 col-sm-1">&nbsp;</div>
-		<div class="col-xs-6 col-sm-3 name">Name</div>
-		<div class="em-col-sm-2 fantasy-points">Fantasy Points</div>
-		<div class="em-col-sm-2 uci-points">UCI Points</div>
-		<div class="em-col-sm-1 rank">Rank</div>
-		<div class="em-col-sm-1 last-race">Last Race</div>
-		<div class="em-col-sm-1 perc-owned">% Owned</div>
-		<div class="em-col-sm-1 cost">Value</div>
+		<div class="col-xs-5 col-sm-3 name">Name</div>
+		<div class="hidden-xs col-sm-2 fantasy-points">Fantasy Points</div>
+		<div class="hidden-xs col-sm-2 uci-points">UCI Points</div>
+		<div class="col-xs-2 col-sm-1 rank">Rank</div>
+		<div class="hidden-xs col-sm-1 last-race">Last Race</div>
+		<div class="hidden-xs col-sm-1 perc-owned">% Owned</div>
+		<div class="col-xs-3 col-sm-1 cost">Value</div>
 	</div>
 
 	<div id="team-riders"></div>
--- fantasy-cycling/js/team-rider-row-content.php
+++ fantasy-cycling/js/team-rider-row-content.php
@@ -1,21 +1,38 @@
 <script type="text/template" id="tmpl-fc-team-rider-row-content">
-	<div class="em-col-sm-4">
-		<span class="add-remove">
-			<a href="">
-				<i class="fa add-rider" aria-hidden="true">Add Rider</i>
-				<i class="fa fa-minus-circle remove-rider" aria-hidden="true"></i>
-			</a>
-		</span>
-		<span class="name"><a href="<%= url %>"><%= name %></a></span>
-		<span class="country"><%= flag %></span>
-	</div>
 
-	<div class="em-col-sm-2 fantasy-points"><%= fantasy_points %></div>
-	<div class="em-col-sm-2 uci-points"><%= uci_points %></div>
-	<div class="em-col-sm-1 rank"><%= rank %></div>
-	<div class="em-col-sm-1 last-race"><a href="<%= last_result.url %>"><%= last_result.place %></a></div>
-	<div class="em-col-sm-1 perc-owned"><%= perc_owned %></div>
-	<div class="em-col-sm-1 cost">
+	<% if (id) { %>
+		<div class="col-xs-2 col-sm-1 add-remove">
+			<span class="add-remove">
+				<a href="">
+					<i class="fa add-rider" aria-hidden="true">
+						<button class="tru-add-riders-btn">Add Rider</button>
+					</i>
+					<i class="fa fa-minus-circle remove-rider" aria-hidden="true"></i>
+				</a>
+			</span>
+		</div>
+
+		<div class="col-xs-5 col-sm-3 name">
+			<a href="<%= url %>"><%= name %></a>
+			<span class="hidden-xs country"><%= flag %></span>
+		</div>
+
+	<% } else { %>
+		<div class="col-xs-12 add-remove">
+			<span class="add-remove">
+				<a href="">
+					<i class="fa add-rider" aria-hidden="true">
+						<button class="tru-add-riders-btn">Add Rider</button>
+					</i>
+					<i class="fa fa-minus-circle remove-rider" aria-hidden="true"></i>
+				</a>
+			</span>
+		</div>
+	<% } %>
+
+	<div class="col-xs-2 rank"><%= rank %></div>
+
+	<div class="col-xs-3 col-sm-2 value">
 		<% if (cost) { %>
 			$
 		<% } %>
@@ -23,5 +40,6 @@
 	</div>
 
 	<input type="hidden" class="rider-id" value="<%= id %>" />
+
 </script>
 
