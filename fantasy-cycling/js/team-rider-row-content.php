14737a0eb919ef7eba018cc03b4605cf162ca49c
erikdmitchell <erikdmitchell@gmail.com>
2016-10-12 15:48:48 -0400

    tweaked my team rider content

--- fantasy-cycling/js/team-rider-row-content.php
+++ fantasy-cycling/js/team-rider-row-content.php
@@ -30,9 +30,12 @@
 		</div>
 	<% } %>
 
-	<div class="col-xs-2 rank"><%= rank %></div>
-
-	<div class="col-xs-3 col-sm-2 value">
+	<div class="hidden-xs col-sm-2 fantasy-points"><%= fantasy_points %></div>
+	<div class="hidden-xs col-sm-2 uci-points"><%= uci_points %></div>
+	<div class="col-xs-2 col-sm-1 rank"><%= rank %></div>
+	<div class="hidden-xs col-sm-1 last-race"><a href="<%= last_result.url %>"><%= last_result.place %></a></div>
+	<div class="hidden-xs col-sm-1 perc-owned"><%= perc_owned %></div>
+	<div class="col-xs-3 col-sm-1 value">
 		<% if (cost) { %>
 			$
 		<% } %>
@@ -42,4 +45,3 @@
 	<input type="hidden" class="rider-id" value="<%= id %>" />
 
 </script>
-
