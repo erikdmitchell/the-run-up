b805ced6aa8d1573097c9dc6332d34fa293146a9
erikdmitchell <erikdmitchell@gmail.com>
2016-10-12 11:44:56 -0400

    added rider and rider in race template

---
+++ fantasy-cycling/js/rider-header.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * template for add rider modal content
+ * these are backbone tempaltes
+ *
+ * It can be overriden
+ *
+ * @since 0.1.0
+ */
+?>
+
+<script type="text/template" id="tmpl-fc-rider-header">
+	<div class="em-col-sm-4 name">Name</div>
+	<div class="em-col-sm-2 fantasy-points">Fantasy Points</div>
+	<div class="em-col-sm-2 uci-points">UCI Points</div>
+	<div class="em-col-sm-1 rank">Rank</div>
+	<div class="em-col-sm-1 last-race">Last Race</div>
+	<div class="em-col-sm-1 perc-owned">Percent Owned</div>
+	<div class="em-col-sm-1 cost">Cost</div>
+</script>
---
+++ fantasy-cycling/js/rider-in-race-header.php
@@ -0,0 +1,9 @@
+<script type="text/template" id="tmpl-fc-rider-in-race-header">
+	<div class="em-col-sm-4 name">Name</div>
+	<div class="em-col-sm-1 proj-finish">Proj Finish</div>
+	<div class="em-col-sm-1 start-pos">Start Pos</div>
+	<div class="em-col-sm-1 last-year">Last Year</div>
+	<div class="em-col-sm-2 fantasy-points">Fantasy Points</div>
+	<div class="em-col-sm-1 rank">Rank</div>
+	<div class="em-col-sm-1 cost">Cost</div>
+</script>
