d22fb55b6bbf73fb1b1b4dbd6284c11378a00bea
Erik <erikdmitchell@gmail.com>
2016-11-29 11:47:35 -0500

    updated and styled recent results

--- fantasy-home/recent-results.php
+++ fantasy-home/recent-results.php
@@ -6,7 +6,7 @@ global $fantasy_cycling_races_with_results;
 	<div class="col-xs-12">
 		<h3>Recent Results</h3>
 
-		<div class="results">
+		<div class="races-recent-results">
 
 			<?php if ($fantasy_cycling_races_with_results) : ?>
 				<div class="header row">
@@ -19,7 +19,7 @@ global $fantasy_cycling_races_with_results;
 				<?php foreach ($fantasy_cycling_races_with_results as $race) : ?>
 					<div id="race-<?php echo $race->id; ?>" class="row">
 						<div class="col-xs-2 race-date"><?php echo date('M. j, Y', strtotime($race->date)); ?></div>
-						<div class="col-xs-6 race-name"><a href="<?php fantasy_cycling_get_race_link($race->code); ?>"><?php echo $race->name; ?></a></div>
+						<div class="col-xs-6 race-name"><a href="<?php echo fantasy_cycling_get_race_link($race->code); ?>"><?php echo $race->name; ?></a></div>
 						<div class="col-xs-2 race-nat"><?php echo $race->nat; ?></div>						
 						<div class="col-xs-2 race-class"><?php echo $race->class; ?></div>
 					</div>
