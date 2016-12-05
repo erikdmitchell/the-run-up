9c5ed48f13757545e8606ff3df567a72aee0819e
Erik <erikdmitchell@gmail.com>
2016-12-05 15:11:42 -0500

    integrated share team on my team page
    fixes issue #50

--- fantasy-cycling/my_team.php
+++ fantasy-cycling/my_team.php
@@ -13,17 +13,33 @@ get_header();
 	<?php fc_my_team_message(); ?>
 
 	<div class="row info">
-		<div class="col-sm-4">
+		<div class="col-xs-12 col-sm-2">
 			<div class="budget">Budget: <span class="amount"><?php echo fantasy_cycling_format_cost($fantasy_cycling_user_team->budget); ?></span></div>
 
 			<div id="my-team-ajax"></div>
 		</div>
-		<div class="col-sm-8">
+		<div class="col-xs-12 col-sm-6">
 			<div class="next-race">
 				<span class="title">This Weeks Races:</span>
 				<?php fc_races_this_week_display(); ?>
 			</div>
 		</div>
+		
+		<div class="tru-social-media-share">		
+			<div class="col-xs-12 col-md-6 share-title">Share Your Team:</div>
+			<div class="col-xs-12 col-md-6 share-btns">
+				<?php do_action( 'addthis_widget', fc_get_team_link($fantasy_cycling_user_team->slug), 'Check out my team: ', array(
+				    'type' => 'custom',
+				    'size' => '32', // size of the icons.  Either 16 or 32
+				    'services' => 'facebook, twitter', // the services you want to always appear
+				    'preferred' => 0, // the number of auto personalized services
+				    'more' => false, // if you want to have a more button at the end
+				    'counter' => false // if you want a counter and the style of it
+				    ));
+				?>
+			</div>
+		</div>		
+		
 	</div>
 
 	<div id="my-team-list" class="my-team-list"></div>
@@ -52,30 +68,23 @@ get_header();
 		</div>
 	</div>
 
-	<div class="tru-social-media-share row">
-		<div class="col-xs-12">
-			<div class="col-xs-6 share-title">Share Your Team:</div>
-			<div class="col-xs-6 share-btns">
-				<?php tru_social_media_share_button(array('url' => fc_get_team_link($fantasy_cycling_user_team->slug), 'text' => 'Check out my team: ')); ?>
-				<?php tru_social_media_share_button(array(
-					'type' => 'facebook',
-					'url' => fc_get_team_link($fantasy_cycling_user_team->slug),
-					'btn_text' => 'Share',
-				)); ?>
-			</div>
+	<div class="tru-social-media-share row">		
+		<div class="col-xs-6 col-sm-3 col-md-2 share-title">Share Your Team:</div>
+		<div class="col-xs-6 col-sm-9 col-md-10 share-btns">
+			<?php do_action( 'addthis_widget', fc_get_team_link($fantasy_cycling_user_team->slug), 'Check out my team: ', array(
+			    'type' => 'custom',
+			    'size' => '32', // size of the icons.  Either 16 or 32
+			    'services' => 'facebook, twitter', // the services you want to always appear
+			    'preferred' => 0, // the number of auto personalized services
+			    'more' => false, // if you want to have a more button at the end
+			    'counter' => false // if you want a counter and the style of it
+			    ));
+			?>
 		</div>
 	</div>
 
 </div>
 
-<?php do_action( 'addthis_widget', get_permalink(), get_the_title(), array(
-    'type' => 'custom',
-    'size' => '16', // size of the icons.  Either 16 or 32
-    'services' => 'facebook,twitter,digg', // the services you want to always appear
-    'preferred' => '7', // the number of auto personalized services
-    'more' => true, // if you want to have a more button at the end
-    'counter' => 'bubble_style' // if you want a counter and the style of it
-    ));
-?>
+
 
 <?php get_footer(); ?>
