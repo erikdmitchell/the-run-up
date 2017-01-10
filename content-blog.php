1f502539a23906db5ecbcd95511d1f5b6d6ede8c
Erik <erikdmitchell@gmail.com>
2017-01-10 16:41:28 -0500

    updating blog page layout

--- content-blog.php
+++ content-blog.php
@@ -6,18 +6,26 @@ $blog_query=new WP_Query(array(
 
 <article id="post-<?php the_ID(); ?>" <?php post_class('tru-blog'); ?>>
 
-	<div class="col-xs-12 col-md-8 main">
+	<div class="content container">
 		<?php if ($blog_query->have_posts()) : ?>
 			<?php while ($blog_query->have_posts()) : $blog_query->the_post(); ?>
-				<?php the_title(); ?>
+			
+				<div class="row">
+					<div class="col-xs-12">
+						<?php the_post_thumbnail(); ?>
+					</div>
+				</div>
+				
+				<div class="row">
+					<div class="col-xs-12">
+						<a href="<?php the_permalink(); ?>"><?php the_title('<h2>', '</h2>'); ?></a>
+					</div>
+				</div>
+			
 			<?php endwhile; ?>
 		<?php else : ?>
 			No blog posts found.
 		<?php endif; ?>
-	</div>
-
-	<div class="col-xs-12 col-md-4 right">
-Sidebar?
 	</div>
 
 </article>
