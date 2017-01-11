005a50f359cbdbadb13cc1a73b10e866207a262a
Erik <erikdmitchell@gmail.com>
2017-01-11 10:18:56 -0500

    styled single blog content template

--- content-single-blog.php
+++ content-single-blog.php
@@ -7,20 +7,44 @@
 
 	<div class="row">
 		<div class="col-xs-12">
-			<?php
-				// Start the Loop.
-				while ( have_posts() ) : the_post();
-					get_template_part('content');
+			<?php while ( have_posts() ) : the_post(); ?>
+					
+				<article id="post-<?php the_ID(); ?>" <?php post_class('tru-blog-single'); ?>>
+					<header class="entry-header">
+						<?php	the_title( '<h1 class="entry-title">', '</h1>' );	?>
+				
+						<div class="entry-meta">
+							<?php the_author(); ?><br />
+							<?php the_time('F j, Y'); ?>
+						</div><!-- .entry-meta -->
+					</header><!-- .entry-header -->
+				
+					<div class="entry-content">
+						<?php
+							the_content( __( 'Continue reading <span class="meta-nav">&raquo;</span>', 'koksijde' ) );
+							wp_link_pages( array(
+								'before'      => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'koksijde' ) . '</span>',
+								'after'       => '</div>',
+								'link_before' => '<span>',
+								'link_after'  => '</span>',
+							) );
+						?>
+					</div><!-- .entry-content -->
+				
+					<?php the_tags( '<footer class="entry-meta"><span class="tag-links">', '', '</span></footer>' ); ?>
+				</article><!-- #post-## -->
 
-					// Previous/next post navigation.
-					koksijde_theme_post_nav();
+				<?php
+				// Previous/next post navigation.
+				//koksijde_theme_post_nav();
 
-					// If comments are open or we have at least one comment, load up the comment template.
-					if ( comments_open() || get_comments_number() ) {
-						comments_template();
-					}
-				endwhile;
-			?>
+				// If comments are open or we have at least one comment, load up the comment template.
+				if ( comments_open() || get_comments_number() ) {
+					comments_template();
+				}
+				?>
+								
+			<?php endwhile; ?>
 		</div>
 	</div>
 </div><!-- .container -->
