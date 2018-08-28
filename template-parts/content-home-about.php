4361d9417d5734ef5be181639d3b522ae2df58ea
Erik <erikdmitchell@gmail.com>
2018-08-28 14:07:26 -0400

    tweaked home about template

---
+++ template-parts/content-home-about.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Home page - about section
+ */
+
+$home_about_query=new WP_Query(array(
+	'posts_per_page' => -1,
+	'post_type' => 'about',
+	'orderby' => 'menu_order',
+	'order' => 'ASC',
+));
+?>
+
+<?php if ($home_about_query->have_posts()) : ?>
+
+	<?php while ($home_about_query->have_posts()) : $home_about_query->the_post(); ?>
+
+		<article id="post-<?php the_ID(); ?>" <?php post_class('blurb col-xs-12 col-sm-6 col-md-4'); ?>>
+			<?php	the_title( '<h2>', '</h2>' );	?>
+
+			<?php	the_content(); ?>
+
+		</article><!-- #post-## -->
+
+	<?php endwhile; ?>
+
+	<?php wp_reset_postdata(); ?>
+
+<?php endif;
