a7acbfc9dc23cedfcdf91fdeae9eecce55d426f5
Erik <erikdmitchell@gmail.com>
2018-08-28 13:04:25 -0400

    fixed thumbnail function

--- template-parts/content-single.php
+++ template-parts/content-single.php
@@ -1,12 +1,12 @@
 <div class="container">
 	<div class="row">
-		<div class="col-md-12">
-			<?php tru_theme_post_thumbnail('single'); ?>
+		<div class="col-12">
+			<?php tru_post_thumbnail('single'); ?>
 		</div>
 	</div>
 
 	<div class="row">
-		<div class="col-xs-12">
+		<div class="col-12">
 			<?php
 				while ( have_posts() ) : the_post();
 					get_template_part('template-parts/', 'content');
