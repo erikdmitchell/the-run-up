673a26549a07d3c13d10f9bf2195961420e00981
Erik <erikdmitchell@gmail.com>
2018-09-03 14:18:33 -0400

    fixed bad function call

--- page-fantasy-main.php
+++ page-fantasy-main.php
@@ -8,7 +8,7 @@ get_header(); ?>
 <div class="container">
 	<div class="row content">
 
-		<?php koksijde_theme_post_thumbnail(); ?>
+		<?php tru_post_thumbnail(); ?>
 
 		<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 			<?php get_template_part('template-parts/content', 'fantasy-landing'); ?>
