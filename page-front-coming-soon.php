f6d7779328c566ebf52a5bed9c89d2290b1e5ba4
Erik <erikdmitchell@gmail.com>
2018-09-04 16:40:53 -0400

    better layout for how to play

--- page-front-coming-soon.php
+++ page-front-coming-soon.php
@@ -41,27 +41,27 @@
 </div>
 
 <div class="container how-to-play">
-	<div class="row">
+	<div class="row justify-content-md-center">
 		<div class="col-md-12 title">
 			How to Play
 		</div>
 		
 		<div class="icons-wrapper">
-    		<div class="col-md-2 step-1">
+    		<div class="col-12 col-md-2 step-1">
                 <i class="material-icons">create</i>
     			<div class="text">Sign up for Free</div>
     		</div>
-    		<div class="col-md-3 arrow">
+    		<div class="col-md-3 d-none d-md-block arrow">
                 <i class="material-icons">chevron_right</i>
     		</div>
-    		<div class="col-md-2 step-2">
+    		<div class="col-12 col-md-2 step-2">
                 <i class="material-icons">people</i>
     			<div class="text">Select riders for your team</div>
     		</div>
-    		<div class="col-md-3 arrow">
+    		<div class="col-md-3 d-none d-md-block arrow">
     			<i class="material-icons">chevron_right</i>
     		</div>
-    		<div class="col-md-2 step-3">
+    		<div class="col-12 col-md-2 step-3">
     			<i class="material-icons">mood</i>
     			<div class="text">Win and brag to your friends</div>
     		</div>
