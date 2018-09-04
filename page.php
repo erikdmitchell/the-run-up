2b88c6421ed96125afcc791ac4e11dced8ecbf35
Erik <erikdmitchell@gmail.com>
2018-09-04 14:07:29 -0400

    added content page template

--- page.php
+++ page.php
@@ -14,7 +14,7 @@ get_header(); ?>
 			<?php tru_post_thumbnail(); ?>
 
 			<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-				<?php get_template_part('template-parts/', 'content'); ?>
+				<?php get_template_part('template-parts/content', 'page'); ?>
 			<?php endwhile; else: ?>
 				<p><?php _e('Sorry, this page does not exist.', 'the-run-up'); ?></p>
 			<?php endif; ?>
