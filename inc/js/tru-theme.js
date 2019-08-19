ed7cabb6c92805b6c5f593fdc1fbc1b2066223c5
Erik <erikdmitchell@gmail.com>
2019-08-19 12:18:05 -0400

    removed nav resize

--- inc/js/tru-theme.js
+++ inc/js/tru-theme.js
@@ -29,38 +29,3 @@ jQuery(document).ready(function($) {
 	});
 
 });
-
-/**
- * nav functions
- */
-jQuery(document).ready(function($) {
-    var $window = $(window);
-    var $navbar = $('.navbar-nav');
-    var $navbarToggle = $('.navbar-toggler');
-    
-    // hamburger nav.
-    $navbarToggle.on('click', function() {        
-        if ($navbar.hasClass('active')) {
-            $navbar.css('display', 'none');
-            $navbar.removeClass('active');
-        } else {
-            $navbar.css('display', 'flex');
-            $navbar.addClass('active');            
-        }
-    });
-    
-    // sets up menu based on window size.
-    function checkWidth() {
-        var windowSize = $window.width();
-        
-        if (windowSize > 768) {
-            $navbar.css('display', 'inline-block');    
-        } else {
-            $navbar.css('display', 'none');
-        }
-    }
-
-    checkWidth();
-    
-    $(window).resize(checkWidth);
-});
