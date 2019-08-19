bd00fb9f3abc896c78817cdaf899da861a32fac4
Erik <erikdmitchell@gmail.com>
2019-08-19 18:58:45 -0400

    cleaned up code style

--- inc/js/tru-theme.js
+++ inc/js/tru-theme.js
@@ -2,30 +2,29 @@
  * back to top button function
  */
 jQuery(document).ready(function($) {
-	// browser window scroll (in pixels) after which the "back to top" link is shown
-	var offset = 300;
-	//browser window scroll (in pixels) after which the "back to top" link opacity is reduced
-	var offset_opacity = 1200;
-	//duration of the top scrolling animation (in ms)
-	var scroll_top_duration = 700;
-	//grab the "back to top" link
-	$back_to_top = $('.tru-back-to-top');
+    // browser window scroll (in pixels) after which the "back to top" link is shown
+    var offset = 300;
+    //browser window scroll (in pixels) after which the "back to top" link opacity is reduced
+    var offset_opacity = 1200;
+    //duration of the top scrolling animation (in ms)
+    var scroll_top_duration = 700;
+    //grab the "back to top" link
+    $back_to_top = $('.tru-back-to-top');
 
-	//hide or show the "back to top" link
-	$(window).scroll(function(){
-		( $(this).scrollTop() > offset ) ? $back_to_top.addClass('btt-is-visible') : $back_to_top.removeClass('btt-is-visible btt-fade-out');
-		if( $(this).scrollTop() > offset_opacity ) {
-			$back_to_top.addClass('btt-fade-out');
-		}
-	});
+    //hide or show the "back to top" link
+    $(window).scroll(function() {
+        ($(this).scrollTop() > offset) ? $back_to_top.addClass('btt-is-visible'): $back_to_top.removeClass('btt-is-visible btt-fade-out');
+        if ($(this).scrollTop() > offset_opacity) {
+            $back_to_top.addClass('btt-fade-out');
+        }
+    });
 
-	//smooth scroll to top
-	$back_to_top.on('click', function(event){
-		event.preventDefault();
-		$('body,html').animate({
-			scrollTop: 0 ,
-		 	}, scroll_top_duration
-		);
-	});
+    //smooth scroll to top
+    $back_to_top.on('click', function(event) {
+        event.preventDefault();
+        $('body,html').animate({
+            scrollTop: 0,
+        }, scroll_top_duration);
+    });
 
 });
