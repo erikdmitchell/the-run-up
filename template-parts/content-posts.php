447a2df1ace7311c5e3e9d752f275249ae4dec8c
Erik <erikdmitchell@gmail.com>
2019-08-27 10:09:36 -0400

    fixed so it uses default wp query

--- template-parts/content-posts.php
+++ template-parts/content-posts.php
@@ -7,25 +7,17 @@
  * @since 1.0.0
  */
 
-$paged = get_query_var( 'paged' ) ? get_query_var( 'paged' ) : 1;
-$blog_query = new WP_Query(
-    array(
-        'posts_per_page' => 12,
-        'offset' => 7,
-        'page' => $paged,
-    )
-);
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class( 'tru-blog-archive' ); ?>>
 
     <div class="content container">
 
-        <?php if ( $blog_query->have_posts() ) : ?>
+        <?php if ( have_posts() ) : ?>
             <div class="row">
                 <?php
-                while ( $blog_query->have_posts() ) :
-                    $blog_query->the_post();
+                while ( have_posts() ) :
+                    the_post();
                     ?>
                 
                     <div class="col-12 col-sm-6">
@@ -45,10 +37,11 @@ $blog_query = new WP_Query(
                 <?php endwhile; ?>
             </div>
         <?php endif; ?>
-
-    
-        <div class="nav-previous alignleft"><?php next_posts_link( 'Older posts', $blog_query->max_num_pages ); ?></div>
+        
+        <div class="nav-previous alignleft"><?php next_posts_link( 'Older posts' ); ?></div>
         <div class="nav-next alignright"><?php previous_posts_link( 'Newer posts' ); ?></div>
+
+        <?php //wp_reset_postdata(); ?>
 
     </div>
 
